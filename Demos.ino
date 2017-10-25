c44e8703fad028bc853ec8352fed2b29eb99cbe6
callimero <c.wartmann@gmx.net>
2017-10-25 15:29:39 +0200

    Makey dazu
    Makey als Vektorgrafik

--- Demos.ino
+++ Demos.ino
@@ -10,14 +10,13 @@ void HalloMake()
 
   // Einfache Supportfunktion um Rechtecke zu zeichnen aus Simple.ino
   draw_rect(0, 0, 4095, 4095);
-
+delay(30);
   // Text/String "Text", X,Y, Groesse
   draw_string("Hallo Make-Magazin!", 100, 150, 6);
 
   // in objects.c definierte Objekte Zeichnen.
-  // Parameter: Objektnummer, X,Y, Größe, Rotation (0== Nach oben zeigend)
-  draw_object(3, 2000, 2000, 100, 0);
-  draw_object(4, 2000, 2000, 70 + rand() % 70 , 0);
+  // Parameter: Objektnummer, X,Y, Größe, Rotation (0== aufrecht/Blender +Y)
+  draw_object(11, 2000, 2000, 40, 0);
 }
 
 
