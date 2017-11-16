b7875261c054d3b3cdc8ab1c3e82fcf370116cd9
callimero <c.wartmann@gmx.net>
2017-11-16 14:56:44 +0100

    Grrr.
    Hoffentlich letzte Änderung...

--- Video.ino
+++ Video.ino
@@ -1,15 +1,8 @@
 // Globale Variablen hier definieren
-int vx = 4; //Geschwindigkeit
-int xpos;
-elapsedMillis frametime;
 
 // wird regelmäßig aufgerufen
 void video()
 {
-  xpos = xpos + vx * frametime;
-  frametime = 0;
-  if (xpos >= 2500 || xpos <= 0)  vx = vx * -1;
-  draw_object(11, 800 + xpos, 2000, 20, 0);       // Lauf Makey lauf!
-  // draw_string("Hallo Make-Magazin!", 500, 2000, 12);
+  HalloMake();
 }
 
